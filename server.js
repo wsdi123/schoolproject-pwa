6ce2089c22617617fb1e3f44ebd50dd3c3859711
unknown <rubenharutyunyandab@gmail.com>
2026-06-25 08:06:42 +0200

    Express server geconfigureerd

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// public map beschikbaar maken
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use("/icons", express.static("icons"));
+
+app.set("view engine", "ejs");
+
+const appNaam = "Healthify";
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home",
+    appNaam,
+  });
+});
+
+app.get("/overzicht", (req, res) => {
+  res.render("overzicht", {
+    title: "Overzicht",
+    appNaam,
+  });
+});
+
+app.get("/toevoegen", (req, res) => {
+  res.render("toevoegen", {
+    title: "Toevoegen",
+    appNaam,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`✅ Server draait op http://localhost:${PORT}`);
+  console.log(`Server draait op http://localhost:${PORT}`);
 });
